d10d22743e1b7d5b2daacb9f614bfe65734e8c5e
Anton vanHooydonk <antonvanhooydonk@gmail.com>
2026-03-03 19:25:03 -0500

    asdf

--- src/main/java/frc/robot/subsystems/fuel/FuelSubsystem.java
+++ src/main/java/frc/robot/subsystems/fuel/FuelSubsystem.java
@@ -36,6 +36,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+
 import frc.robot.Constants.CANConstants;
 import frc.robot.util.Utils;
 
