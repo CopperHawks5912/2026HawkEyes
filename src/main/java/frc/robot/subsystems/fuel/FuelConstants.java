eb284423584990f79a91bb1a4dc2cd7c9ff9a55b
Anton vanHooydonk <antonvanhooydonk@gmail.com>
2026-03-09 21:14:31 -0400

    asdf

--- src/main/java/frc/robot/subsystems/fuel/FuelConstants.java
+++ src/main/java/frc/robot/subsystems/fuel/FuelConstants.java
@@ -16,8 +16,8 @@ public final class FuelConstants {
   // Feeder motor percentages (for brushed motor)
   public static final double kFeederIntakingPercent         = -0.80; // A strong reverse power to pull fuel into the hopper quickly
   public static final double kFeederLaunchingPercent        =  0.60; // Feed fuel into the launcher for launcher
-  public static final double kFeederPassingPercent          =  0.60; // Feed fuel into the launcher for lower power passing
-  public static final double kFeederEjectingPercent         =  0.60; // A moderate reverse power to eject fuel without causing jams
+  public static final double kFeederPassingPercent          =  0.60; // Feed fuel into the launcher for passing
+  public static final double kFeederEjectingPercent         = -0.60; // A moderate reverse power to eject fuel without causing jams
   public static final double kFeederSpinUpPreLaunchPercent  = -0.25; // A small reverse power to hold back fuel while the launcher gets to speed
 
   // Launcher percentages (for intake/eject mode - percentage control)
