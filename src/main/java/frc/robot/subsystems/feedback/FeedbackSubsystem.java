f1e3cc9e46f34b918254fb70ef7d76d4d36b42de
Anton vanHooydonk <antonvanhooydonk@gmail.com>
2026-03-16 22:20:30 -0400

    asdf

--- src/main/java/frc/robot/subsystems/feedback/FeedbackSubsystem.java
+++ src/main/java/frc/robot/subsystems/feedback/FeedbackSubsystem.java
@@ -315,7 +315,7 @@ public class FeedbackSubsystem extends SubsystemBase {
       }
       else {
         // default to off
-        setAllLEDs(Color.kBlack);
+        setAllLEDs(allianceColor);
       }
     }
 
